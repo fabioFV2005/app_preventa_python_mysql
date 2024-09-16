0df52787e4a24dc6a19b064e91f2c452d71a44d1
Fabio <kingofnoobs845@gmail.com>
2024-09-16 10:30:43 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Habilita el modo de depuración automáticamente
+
+@app.route('/index')
+def hello_world():
+    name = "fabito"
+    return render_template('index.html', name=name)
+
+
+#LOGICA PARA INICIAR SESION O REGISTRARSE :D
+@app.route('/')
+def Login():
+    return render_template('auth/Login.html')
+@app.route('/CrearCuenta')
+def Registrar():
+    return render_template('auth/Registro.html')
+
+
+
+
